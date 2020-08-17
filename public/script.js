0c1385058b5a65a15d2cea1207d516555ca2a724
Abbeyme1 <abhy1209120@gmail.com>
2020-08-17 22:34:53 +0530

    started implementing socket.io

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const socket = io("/");
+const myVideo = document.createElement("video");
+const videoGrid = document.getElementById("video-grid");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+  });
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+socket.emit("join-room");
